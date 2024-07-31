68a6f79665c2b1c82fe3919afef5159b922a3e0e
314PiGuy <147645262+314PiGuy@users.noreply.github.com>
2024-07-31 21:34:35 +0530

    Create pipe.hpp

---
+++ pipe.hpp
@@ -0,0 +1,11 @@
+#include <random>
+
+class Pipe{    
+    private:
+    public:
+    double width = 12;
+    double x;
+    double y[2]; // since there are 2 pipes with a gap in them i will just store the x value of pipes start and the y values of both ends
+    Pole();
+    void move();
+};
