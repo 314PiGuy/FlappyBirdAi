6a6e82f95569b7f8c6ba9abe4212d800cefe814f
314PiGuy <147645262+314PiGuy@users.noreply.github.com>
2024-07-31 22:01:51 +0530

    made the move and create scene methods
    I hate my wifi

--- main.cpp
+++ main.cpp
@@ -1,24 +1,69 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include "bird.hpp"
+#include "pipe.hpp"
 
 
 using namespace std;
 using namespace sf;
 
 int cooldown = 0;
-Bird bird = Bird(10) //bird class should have a flap and move method and takes the hitbox length in constructor
+Bird bird = Bird(10); //bird class should have a flap and move method and takes the hitbox length in constructor
 vector<Pipe> pipes; //pipe class represents the pipe obstacle and there will be a vector of the pipes on screen. the pipes should have a move method to move them on the screen
 
+int spawnPipeCountdown = 0; // use this to time when to add a new pipe
+int nextPipe = 0; //index of the pipe in front of the bird
+
+
+
 vector<RectangleShape> createScene(){
-//should return all the elements(bird and pipes) as rectangle shapes to be drawn in main window loop
+    vector<RectangleShape> rects;
+    RectangleShape bird(Vector2f(bird.size, bird.size));
+    bird.setPosition(Vector2f(bird.pos[0], bird.pos[1]));
+    bird.setFillColor(Color::Yellow);
+    rects.push_back(bird);
+    //a rectangle to represent the bird
+
+    int n = 0;
+    for (Pipe p: pipes){
+        //make a rectangle for the top and bottom pipe for each pipe object
+        RectangleShape t(Vector2f(p.width, p.y[0]));
+        t.setPosition(p.x, 0);
+        t.setFillColor(Color::Red);
+        RectangleShape b(Vector2f(p.width, 200-p.y[1]));
+        b.setPosition(p.x, p.y[1]);
+        b.setFillColor(Color::Red);
+        rects.push_back(t);
+        rects.push_back(b);
+        n++;
+    }
+
+    return rects;
 }
 
 void move(){
-//should call the move methods of the bird and pipes
+    for (Pipe& p: pipes){
+        p.move();
+    }
+    bird.move();
+    spawnPipeCountdown--;
+    if (pipes[nextPipe].x+pipes[nextPipe].width < bird.pos[0]){
+        //the next pipe is the next pipe when it passes the bird
+        nextPipe++;
+    }
+    if (pipes[0].x+pipes[0].width < 0){
+        //remove pipes when they go off scree
+        nextPipe--;
+        pipes.erase(pipes.begin());
+    }
+    if (cooldown > 0) cooldown--;
 }
 
 
 int main(){
+
+    srand(time(0));
+    
     RenderWindow window(VideoMode(250, 200), "Bird Up");
 
     window.setFramerateLimit(100);
@@ -42,6 +87,13 @@ int main(){
             cooldown = 30;
         }
 
+        //this makes a pipe every 112 pixels idk why 112 it looks right i suppose
+        if (spawnPipeCountDown == 0){
+            poles.push_back(Pole());
+            spawnPipeCountDown = 112;
+        }
+
+        move();
 
 
         window.clear();
